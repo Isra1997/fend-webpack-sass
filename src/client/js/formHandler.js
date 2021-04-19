4391863c6040c851249e0c5c0f3ded1b7fbc239d
Isra1997 <israragheb@gmail.com>
2021-04-20 00:44:01 +0200

    Added: Weather API call

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -4,16 +4,33 @@ function handleSubmit(event) {
     // check what text was put into the form field
     let formText = document.getElementById('name').value
 
-    Client.checkForName(formText)
+    // Client.checkForName(formText)
 
     console.log("::: Form Submitted :::")
-    fetch('http://localhost:8081/test')
-    .then(res => {
-        return res.json()
+    console.log(formText)
+    // fetch('http://localhost:8081/test')
+    // .then(res => {
+    //     return res.json()
+    // })
+    // .then(function(data) {
+    //     document.getElementById('results').innerHTML = data.message
+    // })
+
+    fetch("//api.openweathermap.org/data/2.5/weather?"+ new URLSearchParams({
+        q: formText,
+        appid:'34aa32cfd675614b3f7a7a29ae6cf373',
+
+    }))
+    .then(resp => {
+           return resp.json()
+        }
+    ).then(function (data) {
+        console.log(data.weather[0].description)
+        document.getElementById('results').innerHTML = data.weather[0].description
     })
-    .then(function(data) {
-        document.getElementById('results').innerHTML = data.message
-    })
+    .catch({function() {
+        console.log("Something went wrong!");
+    }  })
 }
 
 export { handleSubmit }
